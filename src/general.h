3591e7c3366a7701febfe297f9cc0a7a1b2a60f6
Huy Nguyen <huy55465@gmail.com>
2024-06-01 22:30:48 -0400

    feat(cmake): add options to build release or debug version
    I will probably revise how it works soon. Still on my way to learn
    CMake.
    
    commit-by: Huy Nguyen

--- src/general.h
+++ src/general.h
@@ -1,11 +1,10 @@
 #ifndef __SMOLDB_GENERAL_H__
 #define __SMOLDB_GENERAL_H__
 
-#define Smoldb_VERSION_MAJOR 0
-#define Smoldb_VERSION_MINOR 0
+#define Smoldb_VERSION_MAJOR "0"
+#define Smoldb_VERSION_MINOR "0"
 
 #define SMOL_API extern
 #define SMOL_INTERNAL static
 
 #endif // !__SMOLDB_GENERAL_H__
-
