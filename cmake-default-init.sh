9e0a99597febd260614702c4ab7bd0709c45a31b
Huy Nguyen <huy55465@gmail.com>
2024-06-05 10:07:41 -0400

    feat(cmake): add install option
    Running `cmake --install build` after `cmake --build build` at project
    root will put the newly made executable at the project root
    
    Also, added some guides about what to do next after running the cmake
    initialization script.
    
    commit-by: Huy Nguyen

--- cmake-default-init.sh
+++ cmake-default-init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# run this every time you change one of the CMakeLists
 rm -rf ./build compile_commands.json
 mkdir build
 cd build
@@ -7,3 +8,14 @@ cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -G Ninja -DCMAKE_C_COMPILER=clang ..
 cd ..
 ln -s ./build/compile_commands.json compile_commands.json
+
+help_msg="
+what's next?
+
+build the executable:
+  cmake --build build
+get the executable onto the project root:
+  cmake --install build
+"
+
+echo "$help_msg"
