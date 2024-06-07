2880db3ef975e4b0fafdc4841043e170a7b46c1e
Huy Nguyen <huy55465@gmail.com>
2024-06-07 11:33:42 -0400

    feat(sh): update lint and format script
    More explicitly exclude build and include test directory
    
    commit-by: Huy Nguyen

--- clang-format-lint-all.sh
+++ clang-format-lint-all.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-for item in `find . -not -path './build/*' -name '*.c' -or -name '*.h' -or -name '*.h.in'`; do
+find_cmd=`find './src' -name '*.c' -or -name '*.cpp*' -or -name '*.h' -or -name '*.h.in'`
+find_cmd=$find_cmd `find './test' -name '*.c' -or -name '*.cpp*' -or -name '*.h' -or -name '*.h.in'`
+
+for item in $find_cmd; do
   clang-tidy ${item}
   clang-format ${item} -i
 done
