18d65394d2e842ae4a46398d9206b6b5183058d7
Huy Nguyen <huy55465@gmail.com>
2024-06-18 15:19:37 -0400

    delay changes to cmakelist and format and lint script to later

--- clang-format-lint-all.sh
+++ clang-format-lint-all.sh
@@ -4,15 +4,6 @@ find_cmd=`find './src' -name '*.c' -or -name '*.cpp*' -or -name '*.h' -or -name 
 find_cmd=$find_cmd `find './test' -name '*.c' -or -name '*.cpp*' -or -name '*.h' -or -name '*.h.in'`
 
 for item in $find_cmd; do
-  clang-format ${item} -i
   clang-tidy ${item}
-  if (( $? != 0 )); then
-      clang_tidy_ret_code=1
-  fi
+  clang-format ${item} -i
 done
-
-if [ $clang_tidy_ret_code ]; then
-    echo $clang_tidy_ret_code
-    echo "Linter error"
-    exit 1
-fi
