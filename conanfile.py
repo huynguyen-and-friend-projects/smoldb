9049e94d06df2b643f020b8903feb9118bc61ef3
Huy Nguyen <huy55465@gmail.com>
2024-07-24 14:13:21 -0400

    feat: add conanfile to manage deps
    For now, only googletest is managed
    
    commit-by: Huy Nguyen

---
+++ conanfile.py
@@ -0,0 +1,20 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout, CMake 
+
+class SmoldbRecipe(ConanFile):
+    name = "smoldb"
+    version = "0.0.1"
+    settings = "os", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+    options = {
+            "install_gtest": [True, False],
+    }
+    default_options = {
+            "install_gtest": True,
+    }
+
+    def requirements(self):
+        self.requires("gtest/1.14.0")
+
+    def layout(self):
+        cmake_layout(self)
